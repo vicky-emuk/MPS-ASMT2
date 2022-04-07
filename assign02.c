16907bac5437a7776b338516660bf4d0534bf58f
cduggan1 <61393292+cduggan1@users.noreply.github.com>
2022-04-07 12:27:38 +0100

    Update assign02.c

--- assign02.c
+++ assign02.c
@@ -74,6 +74,20 @@ int main() {
     colour_change(4);
     sleep_ms(500);
 
+    //initialise GPIO and bound to variable...
+    asm_gpio_init(22);
+    asm_gpio_init(21);
+    asm_gpio_init(20);
+
+    gpio_set_dir(22, 1);
+    gpio_set_dir(21, 1);
+    gpio_set_dir(20, 1);
+
+    //asm_gpio_get(pin) returns bool value
+    
+
+    
+
     FILE *dictionary;
     dictionary = (fopen("dictionary.txt", "r"));
     if(fptr == NULL)
@@ -100,4 +114,29 @@ int main() {
 
     // Should never get here due to infinite while-loop.
     return 0;
+}
+
+// Initialise a GPIO pin – see SDK for detail on gpio_init()
+void asm_gpio_init(uint pin) {
+    gpio_init(pin);
+}
+
+// Set direction of a GPIO pin – see SDK for detail on gpio_set_dir()
+void asm_gpio_set_dir(uint pin, bool out) {
+    gpio_set_dir(pin, out);
+}
+
+// Get the value of a GPIO pin – see SDK for detail on gpio_get()
+bool asm_gpio_get(uint pin) {
+    return gpio_get(pin);
+}
+
+// Set the value of a GPIO pin – see SDK for detail on gpio_put()
+void asm_gpio_put(uint pin, bool value) {
+    gpio_put(pin, value);
+}
+
+// Enable falling-edge interrupt – see SDK for detail on gpio_set_irq_enabled()
+void asm_gpio_set_irq(uint pin) {
+    gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_FALL, true);
 }
